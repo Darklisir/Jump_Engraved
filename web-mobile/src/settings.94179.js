21b44376e8fb33b58fc5731fc933649869c9ce41
kiluag <kiluag@gmail.com>
2018-01-24 11:03:24 +0800

    “message”

---
+++ web-mobile/src/settings.94179.js
@@ -0,0 +1,207 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "47pc1OtQZICoOZeXiW20oB": [
+                "res/audios/flattening.mp3",
+                2
+            ],
+            "b1o+CJiiVH64YEWYBCCyl0": [
+                "res/audios/jump.mp3",
+                2
+            ],
+            "cdGYnWcZVBlpmdMmxM0UXg": [
+                "res/audios/whatarewords.mp3",
+                2
+            ],
+            "4b2aHgBplAToFG1IFGo83t": [
+                "res/rextures/bg.jpg",
+                0
+            ],
+            "459szqm1JJuraVyhvWF8kv": [
+                "res/rextures/block1.png",
+                0
+            ],
+            "c7Qs9oQy5Df7ReTEw6Qhly": [
+                "res/rextures/block2.png",
+                0
+            ],
+            "73sJbErt1L47OrpL6Zt3Go": [
+                "res/rextures/block3.png",
+                0
+            ],
+            "b8XEOGgcxGkZYhUGLR+4j+": [
+                "res/rextures/dumganhar.png",
+                0
+            ],
+            "ccSpJyrqtKUYhv158l/CNx": [
+                "res/rextures/jare.png",
+                0
+            ],
+            "38F2baov5NiZx8qwr0NgFS": [
+                "res/rextures/knox.png",
+                0
+            ],
+            "1bPFrhOBdLA48GmAxYakM2": [
+                "res/rextures/panda.png",
+                0
+            ],
+            "34MItlDgdESZRgIgt4UQ3T": [
+                "res/rextures/particle_texture.plist",
+                1
+            ],
+            "089Ko9VqtAn5UIVFg0Ks3u": [
+                "res/rextures/particle_texture.png",
+                0
+            ],
+            "96kozNy45GY7PFNqzf6fA2": [
+                "res/rextures/piece.png",
+                0
+            ],
+            "42z5rKtvhDyLeUlLwC2Eov": [
+                "res/rextures/restart_btn.png",
+                0
+            ],
+            "7fDO3O8E1PCaab6lYI0p4P": [
+                "res/rextures/start_btn.png",
+                0
+            ],
+            "36FLCAn8FE2qKKCmCZ9eAt": [
+                "res/rextures/streak.png",
+                0
+            ],
+            "27IdX0Z4REL7aFMF1moKoX": [
+                "res/rextures/wx.jpg",
+                0
+            ],
+            "48DFrn5wdIz6fVA+//wDBD": [
+                "res/rextures/zhe.png",
+                0
+            ]
+        },
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                0
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                0
+            ],
+            "d8HsitJHxOYqo801xBk8ev": [
+                "image/default_panel.png",
+                0
+            ],
+            "4bq2fLGOZAmbhANV8Ec/iQ": [
+                "image/default_scrollbar_bg.png",
+                0
+            ],
+            "02delMVqdBD70a/HSD99FK": [
+                "image/default_sprite_splash.png",
+                0
+            ],
+            "b2aHrECZ5APKGS/0d2hvT1": [
+                "particle/atom.plist",
+                1
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D",
+        "cc.ParticleAsset",
+        "cc.AudioClip"
+    ],
+    launchScene: "db://assets/res/scenes/menu.fire",
+    scenes: [
+        {
+            url: "db://assets/res/scenes/menu.fire",
+            uuid: 3
+        },
+        {
+            url: "db://assets/res/scenes/game.fire",
+            uuid: 0
+        }
+    ],
+    packedAssets: {
+        "016316718": [
+            "01qOXY/z1IX5aQSx/no4k9",
+            "02JzqT2NBEGbATI7kC03Kq",
+            "03AfOqUk9G5qieZfwizhst",
+            "05Rs+tSFNPcrR782MrR/8c",
+            "09bOm7AWJCVZY2z/X+ghkw",
+            "10EMuetPtKQYXi6LjKkLSx",
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "2cqZGCwMNEL4ZiRbJUoK6K",
+            0,
+            "50VkqHc1VCeb00jE5olIow",
+            "51P5r5Ho5P6aMsmZg3b08Q",
+            "5bMKYvpzFNoY1AKoE28IAk",
+            "72sLmtkENNVbR5X0VRkTBy",
+            "7aVMGqqUNHBo/5eNwmx7Zs",
+            1,
+            "85lqfz5IVBuoQPn+G8+Lle",
+            "94xpfoBB1FfIiSkN2r9pdf",
+            2,
+            "a9JaD824FOB7T3428fTJpl",
+            "bbaMJGeStK2LQNW9PhTCrX",
+            "d437hot+xFaJgXpeo84fbr",
+            "d54/fIOGpHCYhsNmcoOMwP",
+            "e6QpjwZXRLm4Q0rJ2ND1pq",
+            "e97GVMl6JHh5Ml5qEDdSGa"
+        ],
+        "042ee5c41": [
+            "3e1fSrGj5K2IoYTUNa12HP",
+            "4cXDRVgXNCIpYbGnWoRUz6",
+            1,
+            "7ciFVBU35K2KuPrgwSWQT6",
+            "9bvaMerUlDyary99mJa6xp",
+            2,
+            "c9+lH/PwFGAY+AMl0bEdq3",
+            3
+        ]
+    },
+    orientation: "portrait",
+    uuids: [
+        "33VomX/5xOGLemLEXossKM",
+        "7bWFQTeX5PTqRWy4jrTR+F",
+        "a2MjXRFdtLlYQ5ouAFv/+R",
+        "f0UsO+Iv5NV5dAy545HZ6u"
+    ],
+    "md5AssetsMap": {
+        "01/016316718.json": "d5248",
+        "04/042ee5c41.json": "2efe2",
+        "assets/res/audios/flattening.mp3": "5348f",
+        "assets/res/audios/jump.mp3": "480ba",
+        "assets/res/audios/whatarewords.mp3": "394b4",
+        "assets/res/rextures/bg.jpg": "e7b3b",
+        "assets/res/rextures/block1.png": "1a255",
+        "assets/res/rextures/block2.png": "9d69c",
+        "assets/res/rextures/block3.png": "71fee",
+        "assets/res/rextures/dumganhar.png": "fc2b2",
+        "assets/res/rextures/jare.png": "bfd77",
+        "assets/res/rextures/knox.png": "10992",
+        "assets/res/rextures/panda.png": "298d5",
+        "assets/res/rextures/particle_texture.plist": "7721b",
+        "assets/res/rextures/particle_texture.png": "5eed4",
+        "assets/res/rextures/piece.png": "d568e",
+        "assets/res/rextures/restart_btn.png": "073e9",
+        "assets/res/rextures/start_btn.png": "6838c",
+        "assets/res/rextures/streak.png": "34961",
+        "assets/res/rextures/wx.jpg": "f2fe8",
+        "assets/res/rextures/zhe.png": "96155",
+        "internal/image/default_btn_disabled.png": "286c6",
+        "internal/image/default_btn_pressed.png": "bedf4",
+        "internal/image/default_panel.png": "cdbc9",
+        "internal/image/default_scrollbar_bg.png": "d6732",
+        "internal/image/default_sprite_splash.png": "cea68",
+        "internal/particle/atom.plist": "ca868"
+    }
+};
