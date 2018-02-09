d9f0dd86dfbd710510441c9e5da6abc5785b0d25
Dakrlisir <lisir_mail@qq.com>
2018-02-09 17:52:37 +0800

    lla

---
+++ src/settings.5e588.js
@@ -0,0 +1,77 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "1e2Ea6YmZEh5kulvKXc7K+": [
+                "image/zhuankuai.png",
+                0
+            ]
+        },
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                0
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                0
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                0
+            ],
+            "6eBWFz0oVHPLIGQKf/9Thu": [
+                "image/default_sprite.png",
+                0
+            ],
+            "02delMVqdBD70a/HSD99FK": [
+                "image/default_sprite_splash.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/brick.fire",
+    scenes: [
+        {
+            url: "db://assets/brick.fire",
+            uuid: 0
+        }
+    ],
+    packedAssets: {
+        "0139a72a0": [
+            "0fTdA5z5pKQ5YJ5JT7xT5J",
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "4dBNtNeHBOkYdmhzOw6+bM",
+            "8c20Sso/ZEn7NUfNSM+EBh",
+            "9bTkbUeq5FUpg/VLYeChoC",
+            "a2MjXRFdtLlYQ5ouAFv/+R",
+            0,
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "f0BIwQ8D5Ml7nTNQbh1YlS"
+        ]
+    },
+    orientation: "",
+    uuids: [
+        "b9oLFfQEBPtrwwYkce4ZRM"
+    ],
+    "md5AssetsMap": {
+        "01/0139a72a0.json": "5d54f",
+        "assets/image/zhuankuai.png": "83b4a",
+        "internal/image/default_btn_disabled.png": "286c6",
+        "internal/image/default_btn_normal.png": "1ecb7",
+        "internal/image/default_btn_pressed.png": "bedf4",
+        "internal/image/default_sprite_splash.png": "cea68",
+        "internal/image/default_sprite.png": "68270"
+    }
+};
